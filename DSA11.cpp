80e91e1ecc8cd7f107fc82fd8fdba2967547dbba
Umesh kumari <umeshkumari123029@gmail.com>
2022-05-04 20:26:23 +0530

    modified linked list to make loop

--- DSA11.cpp
+++ DSA11.cpp
@@ -15,65 +15,44 @@ public:
     }
 };
 
-void insertAfter(node *pre_node, int val)
+void printList(node *head, int total_nodes)
 {
-    node *a = new node(val);
-    a->data = val;
-    a->next = pre_node->next;
-    pre_node->next = a;
-}
-
-void insertAtTail(node *&head, int val)
-{
-
-    node *n = new node(val);
-
-    if (head == NULL)
+    node *curr = head;
+    int count = 0;
+    while (count < total_nodes)
     {
-        head = n;
-        return;
+        count++;
+        cout << curr->data << " ";
+        curr = curr->next;
     }
-    node *temp = head;
-    while (temp->next != NULL)
-    {
-        temp = temp->next;
-    }
-    temp->next = n;
 }
 
-void insertAtHead(node *&head, int val)
+void makeLoop(node **head_ref, int k)
 {
-
-    node *m = new node(val);
-    m->next = head;
-    head = m;
-}
-
-void makeLoop(node *&head, int k){
-
-    node *temp= head;
-    int count=1;
-
-    while(count < k){
+    node *temp = *head_ref;
+    int count = 1;
+    while (count < k)
+    {
         temp = temp->next;
         count++;
     }
 
-    node* join_point = temp;
-    while(temp->next!=NULL){
-        temp=temp->next;
+    node *k_node = temp;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
     }
-
-    temp->next=join_point;
+    temp->next = k_node;
 }
-
+ 
 bool hasCycle(node* head){
+    cout<<endl;
     if(head==NULL){
         cout<<"false"<<endl;
     }
 
     node* slow_ptr = head;
-    node* fast_ptr = head;
+    node* fast_ptr = head->next;
 
     while(slow_ptr!=fast_ptr){
         if(fast_ptr==NULL || fast_ptr->next==NULL){
@@ -83,39 +62,26 @@ bool hasCycle(node* head){
        slow_ptr=slow_ptr->next;
        fast_ptr=fast_ptr->next->next;
     }
-   cout<<"true"<<endl;
+   cout<<" true "<<endl;
 }
-
-void display(node *head)
-{
-    node *temp = head;
-
-    while (temp != NULL)
-    {
-        cout << temp->data << " -> ";
-        temp = temp->next;
-    }
-    cout << endl;
-}
-
-
 
 int main()
 {
-
     node *head = NULL;
-    insertAtTail(head, 4);
-    insertAtTail(head, 1);
-    insertAtTail(head, 54);
-    insertAtTail(head, 2);
-    insertAtTail(head, 9);
-    insertAtTail(head, 19);
-    display(head);
-   // makeLoop(head,3);
-    //display(head);
+    head = new node(3);
+    head->next = new node(7);
+    head->next->next = new node(4);
+    head->next->next->next = new node(9);
+    head->next->next->next->next = new node(2);
+    int k = 2;
+    cout<<endl;
+    cout<<"Given list : ";
+    printList(head,5);
+    makeLoop(&head,k);
+    cout<<endl;
+    cout<<"Modified list : ";
+    printList(head,8);
     hasCycle(head);
 
-
-    return 0;}
-
-
+    return 0;
+}
